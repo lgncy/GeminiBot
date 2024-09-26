4a79416d0e0d66ed4a58a1bfc17ef07120956480
lgncy <wildandwitriatnakusuma3915@gmail.com>
2024-09-26 13:23:51 +0700

    test

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ app = FastAPI()
 orig_origins = [
     "http://localhost:3000",
     "https://porto-wildandwi-git-master-lgncys-projects.vercel.app",
-    "https://porto-wildandwi-bv275qjbo-lgncys-projects.vercel.app"
-    "https://wildandwi.my.id/"  # Ganti dengan URL frontend Anda
+    "https://porto-wildandwi-bv275qjbo-lgncys-projects.vercel.app",  # Tambahkan koma di sini
+    "https://wildandwi.my.id/"
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
